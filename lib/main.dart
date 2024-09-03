eda2e506b7711b07e5d9ac4e4efbff1fd0d58bf7
Nabilah <nabilahsalzu@gmail.com>
2024-09-03 11:22:10 +0800

    Icon and title

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Web App',
+      title: 'MySiswa Admin',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
